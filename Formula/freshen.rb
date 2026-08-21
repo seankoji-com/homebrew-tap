16d6d86b7355e6521ed68fe50c9206a338129de8
Sean Carey <seankojicarey@gmail.com>
2026-08-21 12:41:27 +1000

    feat(formula): update freshen to v0.1.1 (#3)

--- Formula/freshen.rb
+++ Formula/freshen.rb
@@ -1,25 +1,25 @@
 class Freshen < Formula
   desc "TUI dashboard for managing and syncing multi-repo setups"
   homepage "https://github.com/seankoji-com/freshen"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.0/freshen_0.1.0_darwin_arm64.tar.gz"
-      sha256 "28bff8b7f6b64f90af30bbc94b6e57841d14b9d0667c5f0638bae4e3775620de"
+      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.1/freshen_0.1.1_darwin_arm64.tar.gz"
+      sha256 "a0684c68c7ff3e3d540bca630f294567d9feaa9610c74a7d0667cf810b4dfb34"
     else
-      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.0/freshen_0.1.0_darwin_amd64.tar.gz"
-      sha256 "5ac7e568fdbad2b9f488184c447a3b1949cc3bb1408246b56688dcfebe106d15"
+      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.1/freshen_0.1.1_darwin_amd64.tar.gz"
+      sha256 "e9f3897c36fe68223efcd316ad015b56404740f604eb083575e46b85909e9dd3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.0/freshen_0.1.0_linux_arm64.tar.gz"
-      sha256 "2135b474303df082c36a58eba36e20fa4dcbad5b521616dba076995d8d0a40d9"
+      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.1/freshen_0.1.1_linux_arm64.tar.gz"
+      sha256 "2917a1673e6af8b5f9c3b2cb422c3f7412e0dbd62d139ec961438ddcfc664520"
     else
-      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.0/freshen_0.1.0_linux_amd64.tar.gz"
-      sha256 "6c72eb5c637c5a90909247bc33a8876cfa8828eff146b17580bd64f3b21490f7"
+      url "https://github.com/seankoji-com/freshen/releases/download/v0.1.1/freshen_0.1.1_linux_amd64.tar.gz"
+      sha256 "c95fc3f7d62a27b8510eeff24a4a64f5dfb20eb43b3f44c3f7960a77b35dbdec"
     end
   end
 
@@ -28,6 +28,6 @@ class Freshen < Formula
   end
 
   test do
-    assert_match "freshen v0.1.0", shell_output("#{bin}/freshen -v")
+    assert_match "freshen v0.1.1", shell_output("#{bin}/freshen -v")
   end
 end
