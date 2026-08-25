6d532063041d9830ca5004e4a7f8b977d51201ea
Sean Carey <freelance@seankoji.com>
2026-08-26 09:39:14 +1000

    feat(formula): update frugalbar to v0.5.0 (#9)

--- Formula/frugalbar.rb
+++ Formula/frugalbar.rb
@@ -1,9 +1,9 @@
 class Frugalbar < Formula
   desc "Track AI usage & dev limits in the macOS menu bar"
   homepage "https://github.com/seankoji-com/frugalbar"
-  version "0.4.0"
-  url "https://github.com/seankoji-com/frugalbar/releases/download/v0.4.0/frugalbar-v0.4.0-arm64.tar.gz"
-  sha256 "2469b0ece1b740a380af2e6659e9a0bff3fea8fd8b92b44924acdecdc12727e6"
+  version "0.5.0"
+  url "https://github.com/seankoji-com/frugalbar/releases/download/v0.5.0/frugalbar-v0.5.0-arm64.tar.gz"
+  sha256 "6796e968f8f476a8a4f7ef79aa4f1d457b93b40293c73c5f842af56e4b31893b"
 
   depends_on :macos
 
